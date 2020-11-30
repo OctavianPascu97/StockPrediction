3ad2109539db75c713c37bb194b2ccdbe31c0682
opascu <octavian.pascu97@gmail.com>
2020-11-30 17:21:27 +0200

    Initial commit

--- models/nn.py
+++ models/nn.py
@@ -56,33 +56,3 @@ class baselineGRU(nn.Module):
         return out
 
 
-class teacherforcingLSTM(nn.Module):
-    def __init__(self, input_size= input_dim, hidden_size = hidden_dim, output_size = output_dim):
-        super(teacherforcingLSTM, self).__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.lstm = nn.LSTMCell(self.input_size, self.hidden_size)
-        self.linear = nn.Linear(self.hidden_size, self.output_size)
-
-    def forward(self, input, future=0, y=None):
-        outputs = []
-
-        # reset the state of LSTM
-        # the state is kept till the end of the sequence
-        h_t = torch.zeros(input.size(0), self.hidden_size, dtype=torch.float32)
-        c_t = torch.zeros(input.size(0), self.hidden_size, dtype=torch.float32)
-
-        for i, input_t in enumerate(input.chunk(input.size(1), dim=1)):
-            h_t, c_t = self.lstm(input_t, (h_t, c_t))
-            output = self.linear(h_t)
-            outputs += [output]
-
-        for i in range(future):  # teacher forcing
-            if y is not None and random.random() > 0.5:
-                output = y[:, [i]]
-            h_t, c_t = self.lstm(output, (h_t, c_t))
-            output = self.linear(h_t)
-            outputs += [output]
-        outputs = torch.stack(outputs, 1).squeeze(2)
-        return outputs
